875b3d9b91808738864c5f03cab4b5613d94fc71
Lucas <lucasmaggi03@gmail.com>
2025-06-03 18:40:02 -0300

    first commit

---
+++ src/components/CriptoSearchForm.tsx
@@ -0,0 +1,25 @@
+import {currencies} from '../data'
+
+export default function CriptoSearchForm() {
+  return (
+    <form className="form">
+        <div className="field">
+            <label htmlFor="currency">Moneda:</label>
+            <select name="currency" id="currency">
+                <option value="">Seleccione</option>
+            </select>
+        </div>
+
+        <div className="field">
+            <label htmlFor="cryptocurrency">Criptomoneda:</label>
+            <select name="cryptocurrency" id="cryptocurrency">
+                <option value="">Seleccione</option>
+                {currencies.map(currency => (
+                    <option key={currency.code} value={currency.code}>{currency.name}</option>
+                ))}
+            </select>
+        </div>
+        <input type="submit" value='Cotizar'/>
+    </form>
+  )
+}
